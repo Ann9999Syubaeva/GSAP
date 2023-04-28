13d235f5c77afc7ebe4334d3f31645b91e437706
Ann9999Syubaeva <syubaeva9999@gmail.com>
2023-04-29 02:51:20 +0300

    1
    1

---
+++ js/app.js
@@ -0,0 +1,52 @@
+/* Плавный скролл страницы */
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !==1) {
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+
+	/* Без h1 заголовка */
+	gsap.fromTo('.hero-section', { opacity: 1 }, {
+		opacity: 0, 
+		scrollTrigger: {
+			trigger: '.hero-section',
+			start: 'center',
+			end: '1000',
+			scrub: true
+		}
+	})
+
+	/* Левая часть */
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+	itemsL.forEach(item => {
+		gsap.fromTo(item, { x: -50, opacity: 0}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	/* Правая часть */
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, { x: 50, opacity: 0}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+}
